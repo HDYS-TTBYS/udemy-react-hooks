3beff1f7c345a90117f1d7b3374cbe5929003207
HDYS-TTBYS <tatehide15@outlook.jp>
2020-02-13 21:09:53 +0900

    imprement event creation

--- src/reducers/reducer_index.js
+++ src/reducers/reducer_index.js
@@ -1,21 +1,26 @@
-const events = (state = [], action) => {};
-switch (action.type) {
-    case 'CREATE_EVENT':
-        const event = { title: action.title, body: action.body };
-        const length = state.length;
-        let id;
-        if (length === 0) {
-            id = 1;
-        } else {
-            id = state[length - 1].id + 1;
-        }
-        return [...state, { id: id, ...event }];
-    case 'DELETE_EVENT':
-        return state;
-    case 'DERETE_ALL_EVENT':
-        return [];
+const events = (state = [], action) => {
+    switch (action.type) {
+        case 'CREATE_EVENT':
+            const event = {
+                title: action.title,
+                body: action.body,
+            };
+            const length = state.length;
+            let id;
+            if (length === 0) {
+                id = 1;
+            } else {
+                id = state[length - 1].id + 1; //最後の要素+1
+            }
+            return [...state, { id: id, ...event }]; //stateに追加
+        case 'DELETE_EVENT':
+            return state;
+        case 'DERETE_ALL_EVENT':
+            return [];
 
-    default:
-        return state;
-}
+        default:
+            return state;
+    }
+};
+
 export default events;
