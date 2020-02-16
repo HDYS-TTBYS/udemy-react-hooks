bb6b2f7dbd66304be33e922c6ed054a1bcbc4b58
HDYS-TTBYS <tatehide15@outlook.jp>
2020-02-16 14:12:52 +0900

    setpu react context privider

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import EventForm from './EventForm';
 import Events from './Events';
+import AppConttext from '../contexts/AppContext';
 import reducer from '../reducers/reducer_index';
+
+console.log({ AppConttext });
 
 const App = () => {
     const [state, dispatch] = useReducer(reducer, []);
 
     return (
         <>
-            <div className="container-fluid">
-                <EventForm state={state} dispatch={dispatch} />
-                <Events state={state} dispatch={dispatch} />
-            </div>
+            <AppConttext.Provider value={'Hello I am a provider'}>
+                <div className="container-fluid">
+                    <EventForm state={state} dispatch={dispatch} />
+                    <Events state={state} dispatch={dispatch} />
+                </div>
+            </AppConttext.Provider>
         </>
     );
 };
